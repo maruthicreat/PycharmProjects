0f113f1e800400be4a516759def79f57ec74dc92
maruthicreat <maruthicreat@gmail.com>
2018-02-07 00:03:24 +0530

    firebase

--- untitled/.idea/firebaseconnect.py
+++ untitled/.idea/firebaseconnect.py
@@ -1,4 +1,7 @@
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://smartshoppingfinal.firebaseio.com/', None)
-result = firebase.get('/CustomerSignup','2oboeR8MUNe1eJsTkogZZAj9c0D3')
+#result = firebase.get('/CustomerSignup','2oboeR8MUNe1eJsTkogZZAj9c0D3')
+#print(result)
+new_user = 'it is soo good'
+result = firebase.post('/Review', {'print': 'silent','url':'hai'})
 print(result)
